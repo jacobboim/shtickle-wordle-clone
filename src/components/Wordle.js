258450dad0a161386c704d20e5846ef88e1b51a6
jacobboim <57962931+jacobboim@users.noreply.github.com>
2022-08-12 09:50:08 -0400

    fix height

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -13,6 +13,7 @@ import blackSunFilled from "../images/blackSunFilled.svg";
 import whiteSunFilled from "../images/whiteSunFilled.svg";
 import newBlackSunSvg from "../images/newBlackSunSvg.svg";
 import newWhiteSunSvg from "../images/newWhiteSunSvg.svg";
+import s from "../images/My project-1_adobe_express.svg";
 import useLocalStorage from "use-local-storage";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
